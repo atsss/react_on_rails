93530732170cfd2aafab971eda533329a13fd30f
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-13 00:18:03 +0900

    set slim in view

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'coffee-rails', '~> 4.2'
 # gem 'therubyracer', platforms: :ruby
 # Use react
 gem 'react-rails'
+# Use slim
+gem 'slim-rails'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,7 @@ Bundler.require(*Rails.groups)
 
 module ReactOnRails
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators.template_engine = :slim
   end
 end
+
