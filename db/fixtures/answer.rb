49f3020ff6a3762b7198bed463ecc75f06864e46
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-13 09:30:53 +0900

    create seed file of question and answer

---
+++ db/fixtures/answer.rb
@@ -0,0 +1,45 @@
+Answer.seed(:id,
+  {
+    :id => 1,
+    :question_id => 1,
+    :content => <<~EOT
+  answer test 1
+  EOT
+  },
+  {
+    :id => 2,
+    :question_id => 1,
+    :content => <<~EOT
+    answer test 2
+  EOT
+  },
+  {
+    :id => 3,
+    :question_id => 1,
+    :content => <<~EOT
+    answer test 3
+  EOT
+  },
+  {
+    :id => 4,
+    :question_id => 2,
+    :content => <<~EOT
+    answer test 4
+  EOT
+  },
+  {
+    :id => 5,
+    :question_id => 2,
+    :content => <<~EOT
+    answer test 5
+  EOT
+  },
+  {
+    :id => 6,
+    :question_id => 3,
+    :content => <<~EOT
+    answer test 6
+  EOT
+  }
+)
+
---
+++ db/fixtures/question.rb
@@ -0,0 +1,45 @@
+Question.seed(:id,
+  {
+    :id => 1,
+    :title => 'angular2 is fun？',
+    :content => <<~EOT
+    question test 1
+  EOT
+  },
+  {
+    :id => 2,
+    :title => 'rails5 is uesful?',
+    :content => <<~EOT
+    question test 2
+  EOT
+  },
+  {
+    :id => 3,
+    :title => 'how can i get up early?',
+    :content => <<~EOT
+    question test 3
+  EOT
+  },
+  {
+    :id => 4,
+    :title => 'hogehoge',
+    :content => <<~EOT
+    question test 4
+  EOT
+  },
+  {
+    :id => 5,
+    :title => 'fugafuga',
+    :content => <<~EOT
+    question test 5
+  EOT
+  },
+  {
+    :id => 6,
+    :title => 'piyopiyo',
+    :content => <<~EOT
+    question test 6
+  EOT
+  }
+)
+
