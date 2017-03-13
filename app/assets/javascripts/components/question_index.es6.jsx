b6d5bb0d844c84cf1f81f001b86fea62bdb53b78
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-14 00:28:55 +0900

    display sample component

---
+++ app/assets/javascripts/components/question_index.es6.jsx
@@ -0,0 +1,10 @@
+class QuestionIndex extends React.Component {
+  render () {
+    return(
+      <div className="questionIndex">
+          Hello, world! Im QuestionIndex Component.
+      </div>
+    );
+  }
+}
+
